817909d49b496416ec8a575da9752b834128bda9
Grant Miller <GrantM11235@gmail.com>
2021-01-04 22:37:23 -0600

    cleanup

--- src/dma.rs
+++ src/dma.rs
@@ -1,11 +1,6 @@
 //! # Direct Memory Access
 #![allow(dead_code)]
 
-use core::{
-    marker::PhantomData,
-    mem, ptr,
-    sync::atomic::{self, compiler_fence, Ordering},
-};
 use embedded_dma::{StaticReadBuffer, StaticWriteBuffer};
 
 use crate::rcc::AHB;
@@ -16,9 +11,6 @@ pub enum Error {
     Overrun,
     TransferError,
 }
-
-type Result<T> = core::result::Result<T, Error>;
-type ResultNonBlocking<T> = nb::Result<T, Error>;
 
 pub enum Event {
     HalfTransfer,
@@ -31,90 +23,23 @@ pub enum Half {
     Second,
 }
 
-pub struct CircBuffer<BUFFER, PAYLOAD>
-where
-    BUFFER: 'static,
-{
-    buffer: &'static mut [BUFFER; 2],
-    payload: PAYLOAD,
-    readable_half: Half,
-}
-
-impl<BUFFER, PAYLOAD> CircBuffer<BUFFER, PAYLOAD>
-where
-    &'static mut [BUFFER; 2]: StaticWriteBuffer,
-    BUFFER: 'static,
-{
-    pub(crate) fn new(buf: &'static mut [BUFFER; 2], payload: PAYLOAD) -> Self {
-        CircBuffer {
-            buffer: buf,
-            payload,
-            readable_half: Half::Second,
-        }
+bitflags::bitflags! {
+    pub struct Flags: u32 {
+        const GLOBAL = 0b0001;
+        const TRANSFER_COMPLETE = 0b0010;
+        const HALF_TRANSFER = 0b0100;
+        const TRANSFER_ERROR = 0b1000;
     }
 }
+
+type Result<T> = core::result::Result<T, Error>;
+type ResultNonBlocking<T> = nb::Result<T, Error>;
 
 pub trait DmaExt {
     type Channels;
 
     fn split(self, ahb: &mut AHB) -> Self::Channels;
 }
-
-pub trait TransferPayload {
-    fn start(&mut self);
-    fn stop(&mut self);
-}
-
-pub struct Transfer<MODE, BUFFER, PAYLOAD>
-where
-    PAYLOAD: TransferPayload,
-{
-    _mode: PhantomData<MODE>,
-    buffer: BUFFER,
-    payload: PAYLOAD,
-}
-
-impl<BUFFER, PAYLOAD> Transfer<R, BUFFER, PAYLOAD>
-where
-    PAYLOAD: TransferPayload,
-{
-    pub(crate) fn r(buffer: BUFFER, payload: PAYLOAD) -> Self {
-        Transfer {
-            _mode: PhantomData,
-            buffer,
-            payload,
-        }
-    }
-}
-
-impl<BUFFER, PAYLOAD> Transfer<W, BUFFER, PAYLOAD>
-where
-    PAYLOAD: TransferPayload,
-{
-    pub(crate) fn w(buffer: BUFFER, payload: PAYLOAD) -> Self {
-        Transfer {
-            _mode: PhantomData,
-            buffer,
-            payload,
-        }
-    }
-}
-
-impl<MODE, BUFFER, PAYLOAD> Drop for Transfer<MODE, BUFFER, PAYLOAD>
-where
-    PAYLOAD: TransferPayload,
-{
-    fn drop(&mut self) {
-        self.payload.stop();
-        compiler_fence(Ordering::SeqCst);
-    }
-}
-
-/// Read transfer
-pub struct R;
-
-/// Write transfer
-pub struct W;
 
 macro_rules! dma {
     ($($DMAX:ident: ($dmaX:ident, {
@@ -242,72 +167,6 @@ dma! {
     }),
 }
 
-/// DMA Receiver
-pub struct RxDma<PAYLOAD, RXCH> {
-    pub(crate) payload: PAYLOAD,
-    pub channel: RXCH,
-}
-
-/// DMA Transmitter
-pub struct TxDma<PAYLOAD, TXCH> {
-    pub(crate) payload: PAYLOAD,
-    pub channel: TXCH,
-}
-
-/// DMA Receiver/Transmitter
-pub struct RxTxDma<PAYLOAD, RXCH, TXCH> {
-    pub(crate) payload: PAYLOAD,
-    pub rxchannel: RXCH,
-    pub txchannel: TXCH,
-}
-
-pub trait Receive {
-    type RxChannel;
-    type TransmittedWord;
-}
-
-pub trait Transmit {
-    type TxChannel;
-    type ReceivedWord;
-}
-
-/// Trait for circular DMA readings from peripheral to memory.
-pub trait CircReadDma<B, RS>: Receive
-where
-    &'static mut [B; 2]: StaticWriteBuffer<Word = RS>,
-    B: 'static,
-    Self: core::marker::Sized,
-{
-    fn circ_read(self, buffer: &'static mut [B; 2]) -> CircBuffer<B, Self>;
-}
-
-/// Trait for DMA readings from peripheral to memory.
-pub trait ReadDma<B, RS>: Receive
-where
-    B: StaticWriteBuffer<Word = RS>,
-    Self: core::marker::Sized + TransferPayload,
-{
-    fn read(self, buffer: B) -> Transfer<W, B, Self>;
-}
-
-/// Trait for DMA writing from memory to peripheral.
-pub trait WriteDma<B, TS>: Transmit
-where
-    B: StaticReadBuffer<Word = TS>,
-    Self: core::marker::Sized + TransferPayload,
-{
-    fn write(self, buffer: B) -> Transfer<R, B, Self>;
-}
-
-bitflags::bitflags! {
-    pub struct Flags: u32 {
-        const GLOBAL = 0b0001;
-        const TRANSFER_COMPLETE = 0b0010;
-        const HALF_TRANSFER = 0b0100;
-        const TRANSFER_ERROR = 0b1000;
-    }
-}
-
 pub trait ChannelLowLevel: Sized {
     /// Associated peripheral `address`
     fn set_peripheral_address(&mut self, address: u32);
@@ -401,183 +260,6 @@ pub trait ChannelLowLevel: Sized {
     }
 }
 
-impl<B, PAYLOAD, CX> CircBuffer<B, RxDma<PAYLOAD, CX>>
-where
-    CX: ChannelLowLevel,
-    RxDma<PAYLOAD, CX>: TransferPayload,
-{
-    /// Peeks into the readable half of the buffer
-    pub fn peek<R, F>(&mut self, f: F) -> Result<R>
-    where
-        F: FnOnce(&B, Half) -> R,
-    {
-        let half_being_read = self.readable_half()?;
-
-        let buf = match half_being_read {
-            Half::First => &self.buffer[0],
-            Half::Second => &self.buffer[1],
-        };
-
-        // XXX does this need a compiler barrier?
-        let ret = f(buf, half_being_read);
-
-        let flags = self.payload.channel.get_flags();
-        let first_half_is_done = flags.contains(Flags::HALF_TRANSFER);
-        let second_half_is_done = flags.contains(Flags::TRANSFER_COMPLETE);
-
-        if (half_being_read == Half::First && second_half_is_done)
-            || (half_being_read == Half::Second && first_half_is_done)
-        {
-            Err(Error::Overrun)
-        } else {
-            Ok(ret)
-        }
-    }
-
-    /// Returns the `Half` of the buffer that can be read
-    pub fn readable_half(&mut self) -> Result<Half> {
-        let flags = self.payload.channel.get_flags();
-        let first_half_is_done = flags.contains(Flags::HALF_TRANSFER);
-        let second_half_is_done = flags.contains(Flags::TRANSFER_COMPLETE);
-
-        if first_half_is_done && second_half_is_done {
-            return Err(Error::Overrun);
-        }
-
-        let last_read_half = self.readable_half;
-
-        Ok(match last_read_half {
-            Half::First => {
-                if second_half_is_done {
-                    self.payload.channel.clear_flags(Flags::TRANSFER_COMPLETE);
-
-                    self.readable_half = Half::Second;
-                    Half::Second
-                } else {
-                    last_read_half
-                }
-            }
-            Half::Second => {
-                if first_half_is_done {
-                    self.payload.channel.clear_flags(Flags::HALF_TRANSFER);
-
-                    self.readable_half = Half::First;
-                    Half::First
-                } else {
-                    last_read_half
-                }
-            }
-        })
-    }
-
-    /// Stops the transfer and returns the underlying buffer and RxDma
-    pub fn stop(mut self) -> (&'static mut [B; 2], RxDma<PAYLOAD, CX>) {
-        self.payload.stop();
-
-        (self.buffer, self.payload)
-    }
-}
-
-impl<BUFFER, PAYLOAD, MODE, CX> Transfer<MODE, BUFFER, RxDma<PAYLOAD, CX>>
-where
-    CX: ChannelLowLevel,
-    RxDma<PAYLOAD, CX>: TransferPayload,
-{
-    pub fn is_done(&self) -> bool {
-        !self.payload.channel.in_progress()
-    }
-
-    pub fn wait(mut self) -> (BUFFER, RxDma<PAYLOAD, CX>) {
-        while !self.is_done() {}
-
-        atomic::compiler_fence(Ordering::Acquire);
-
-        self.payload.stop();
-
-        // we need a read here to make the Acquire fence effective
-        // we do *not* need this if `dma.stop` does a RMW operation
-        unsafe {
-            ptr::read_volatile(&0);
-        }
-
-        // we need a fence here for the same reason we need one in `Transfer.wait`
-        atomic::compiler_fence(Ordering::Acquire);
-
-        // `Transfer` needs to have a `Drop` implementation, because we accept
-        // managed buffers that can free their memory on drop. Because of that
-        // we can't move out of the `Transfer`'s fields, so we use `ptr::read`
-        // and `mem::forget`.
-        //
-        // NOTE(unsafe) There is no panic branch between getting the resources
-        // and forgetting `self`.
-        unsafe {
-            let buffer = ptr::read(&self.buffer);
-            let payload = ptr::read(&self.payload);
-            mem::forget(self);
-            (buffer, payload)
-        }
-    }
-}
-
-impl<BUFFER, PAYLOAD, MODE, CX> Transfer<MODE, BUFFER, TxDma<PAYLOAD, CX>>
-where
-    CX: ChannelLowLevel,
-    TxDma<PAYLOAD, CX>: TransferPayload,
-{
-    pub fn is_done(&self) -> bool {
-        !self.payload.channel.in_progress()
-    }
-
-    pub fn wait(mut self) -> (BUFFER, TxDma<PAYLOAD, CX>) {
-        while !self.is_done() {}
-
-        atomic::compiler_fence(Ordering::Acquire);
-
-        self.payload.stop();
-
-        // we need a read here to make the Acquire fence effective
-        // we do *not* need this if `dma.stop` does a RMW operation
-        unsafe {
-            ptr::read_volatile(&0);
-        }
-
-        // we need a fence here for the same reason we need one in `Transfer.wait`
-        atomic::compiler_fence(Ordering::Acquire);
-
-        // `Transfer` needs to have a `Drop` implementation, because we accept
-        // managed buffers that can free their memory on drop. Because of that
-        // we can't move out of the `Transfer`'s fields, so we use `ptr::read`
-        // and `mem::forget`.
-        //
-        // NOTE(unsafe) There is no panic branch between getting the resources
-        // and forgetting `self`.
-        unsafe {
-            let buffer = ptr::read(&self.buffer);
-            let payload = ptr::read(&self.payload);
-            mem::forget(self);
-            (buffer, payload)
-        }
-    }
-}
-
-impl<BUFFER, PAYLOAD, CX> Transfer<W, BUFFER, RxDma<PAYLOAD, CX>>
-where
-    CX: ChannelLowLevel,
-    RxDma<PAYLOAD, CX>: TransferPayload,
-{
-    pub fn peek<T>(&self) -> &[T]
-    where
-        BUFFER: AsRef<[T]>,
-    {
-        let pending = self.payload.channel.get_ndtr() as usize;
-
-        let slice = self.buffer.as_ref();
-        let capacity = slice.len();
-
-        &slice[..(capacity - pending)]
-    }
-}
-
 pub trait DmaWord {
     const SIZE: crate::pac::dma1::ch::cr::PSIZE_A;
 }
@@ -594,16 +276,22 @@ impl DmaWord for u32 {
     const SIZE: crate::pac::dma1::ch::cr::PSIZE_A = crate::pac::dma1::ch::cr::PSIZE_A::BITS32;
 }
 
+/// Read transfer
+pub struct Rx;
+
+/// Write transfer
+pub struct Tx;
+
 pub trait Direction {
     const DIRECTION: stm32f1::stm32f103::dma1::ch::cr::DIR_A;
 }
 
-impl Direction for R {
+impl Direction for Rx {
     const DIRECTION: stm32f1::stm32f103::dma1::ch::cr::DIR_A =
         stm32f1::stm32f103::dma1::ch::cr::DIR_A::FROMPERIPHERAL;
 }
 
-impl Direction for W {
+impl Direction for Tx {
     const DIRECTION: stm32f1::stm32f103::dma1::ch::cr::DIR_A =
         stm32f1::stm32f103::dma1::ch::cr::DIR_A::FROMMEMORY;
 }
@@ -644,7 +332,7 @@ where
 impl<CHANNEL, PERIPH> PeriphChannel<CHANNEL, PERIPH>
 where
     CHANNEL: ChannelLowLevel,
-    PERIPH: DmaPeriph<Direction = R>,
+    PERIPH: DmaPeriph<Direction = Rx>,
 {
     pub fn recieve_linear<BUFFER>(
         mut self,
@@ -685,7 +373,7 @@ where
 impl<CHANNEL, PERIPH> PeriphChannel<CHANNEL, PERIPH>
 where
     CHANNEL: ChannelLowLevel,
-    PERIPH: DmaPeriph<Direction = W>,
+    PERIPH: DmaPeriph<Direction = Tx>,
 {
     pub fn send_linear<B>(mut self, mut buffer: B) -> LinearTransfer<CHANNEL, PERIPH, B>
     where
